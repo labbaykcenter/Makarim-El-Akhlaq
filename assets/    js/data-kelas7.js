b88e6b53aca3bddda6bcb8fd7abebf0180b2e10f
labbaykcenter <bayuassallaatoh@gmail.com>
2026-02-26 06:59:29 +0700

    Add data-kelas7.js with student scores

---
+++ assets/    js/data-kelas7.js
@@ -0,0 +1,14 @@
+const dataNilai = [
+  {
+    nama: "Ahmad Fauzi",
+    fiqih: 88,
+    nahwu: 85,
+    tahfizh: 90
+  },
+  {
+    nama: "Abdullah",
+    fiqih: 82,
+    nahwu: 80,
+    tahfizh: 89
+  }
+];
